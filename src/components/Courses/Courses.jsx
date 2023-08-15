ede461e92a43bd12554e48302f791c56105c0303
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-15 12:17:08 +0300

    course

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Courses() {
+  return (
+    <div>
+        <h1>Courses We Offer</h1>
+        <p>lorem djhf wfhf wfn whfiheow 3</p>
+    </div>
+  )
+}
+
+export default Courses
+
